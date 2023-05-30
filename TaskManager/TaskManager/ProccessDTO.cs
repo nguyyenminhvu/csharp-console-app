07adffabc8d18ce255f791e1b4c8f25a46f8374b
nguyyenminhvuuu <nguyyenminhvu@gmail.com>
2023-05-31 05:53:52 +0700

    update

---
+++ TaskManager/TaskManager/ProccessDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class ProccessDTO
+    {
+        public ProccessDTO() { }
+        public string Name { get; set; }    
+        public string Id { get; set; }
+        public ProccessDTO(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+
+namespace TaskManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               while (true)
+                {
+                Dictionary<BigInteger, String> processMap = new Dictionary<BigInteger, String>();
+                Process[] processesApp = Process.GetProcesses();
+                foreach (Process item in processesApp)
+                {
+                    Console.WriteLine($"ID: {item.Id}, Name: {item.ProcessName}");
+                    processMap.Add(item.Id, item.ProcessName);
+                }
+                Console.WriteLine("Press D to end task proccess.");
+                if (Console.Read() == 'd')
+                {
+                    Console.WriteLine("ID Task?");
+                    Console.ReadLine();
+                    int d = Convert.ToInt32(Console.ReadLine());
+                    Process.GetProcessById(d).Kill();
+                    Console.WriteLine("Deleted");
+                }
+            }
+        }
+    }
+}
